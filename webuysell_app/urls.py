a3913568b0456b362f94915d919f7cac05af3c8f
kbtiger88 <77023122+kbtiger88@users.noreply.github.com>
2021-04-23 18:06:28 -0500

    added product and add_product urls on urls.py

--- webuysell_app/urls.py
+++ webuysell_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index),
     path('register', views.register),
     path('login', views.login),
+    path('product/<int:product_id>', views.product),
+    path('add_product', views.add_product),
 ]
